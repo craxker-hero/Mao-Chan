da418226ea2ce3886a07e0925ba5ebba3b1a5b0b
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-08-11 18:36:06 -0400

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
 import fs from 'fs'
 
-// Forzar carpeta temporal a ./tmp
 process.env.TMPDIR = path.join(process.cwd(), 'tmp')
 
-// Crear la carpeta si no existe
 if (!fs.existsSync(process.env.TMPDIR)) {
   fs.mkdirSync(process.env.TMPDIR, { recursive: true })
 }
